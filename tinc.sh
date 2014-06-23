eff39509e531f30390b2e09315978f0aeeaa1fca
lancethepants <lancethepants@gmail.com>
2014-06-23 07:18:26 -0600

    Update OpenSSL to latest. 1.0.1h.

--- tinc.sh
+++ tinc.sh
@@ -62,9 +62,9 @@ make install DESTDIR=$BASE
 ########### #################################################################
 
 mkdir -p $SRC/openssl && cd $SRC/openssl
-$WGET http://www.openssl.org/source/openssl-1.0.1g.tar.gz
-tar zxvf openssl-1.0.1g.tar.gz
-cd openssl-1.0.1g
+$WGET http://www.openssl.org/source/openssl-1.0.1h.tar.gz
+tar zxvf openssl-1.0.1h.tar.gz
+cd openssl-1.0.1h
 
 cat << "EOF" > openssl.patch
 --- Configure_orig      2013-11-19 11:32:38.755265691 -0700
